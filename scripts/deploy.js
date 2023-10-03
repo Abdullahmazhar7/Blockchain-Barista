bf4b2ac8ee09eeadd7d88b4ed1e38c2a67a22b43
Abdullahmazhar7 <107640362+Abdullahmazhar7@users.noreply.github.com>
2023-10-03 10:34:50 +0500

    Create a decentralized Coffee app

---
+++ scripts/deploy.js
@@ -0,0 +1,19 @@
+const hre = require("hardhat");
+
+async function main() {
+  const Chai = await hre.ethers.getContractFactory("chai"); //fetching bytecode and ABI
+  const chai = await Chai.deploy(); //creating an instance of our smart contract
+
+  await chai.deployed();//deploying your smart contract
+
+  console.log("Deployed contract address:",`${chai.address}`);
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
+ //0x9bbE20d1b3AC606C207202a786c6D759188AEc82
+ 
